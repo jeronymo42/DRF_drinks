c9ab505afa3263f7a04abdd10f90a4efb7a9d69b
V.Ledovskikh <vladimir.ledovskikh@gmail.com>
2023-10-02 23:44:35 +0300

    add DRF Hello-world function

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
+
+@api_view(['GET'])
+def index(request):
+    return Response({'test': 'hell_world'})
